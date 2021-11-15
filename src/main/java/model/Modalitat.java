a211d29647b6963a28f29d141c1ade07e8ca9929
TirBaGo <tirbago.hometraining@gmail.com>
2021-11-15 22:51:24 +0100

    Commit add Modalitat

--- src/main/java/model/Modalitat.java
+++ src/main/java/model/Modalitat.java
@@ -5,11 +5,64 @@
  */
 package model;
 
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 /**
  *
  * @author Nerea Gallardo
  * @version 1.0
  */
-public class Modalitat {
+@Entity
+@Table(name = "modalitat")
+public class Modalitat implements Serializable{
+    private static final long serialVersionUID = 1L;
+    
+    
+    
+    @NotNull
+    @Column(name = "nom")
+    @Size(max = 255)
+    @Id
+    private String nom;
+    
+    @NotNull
+    @Column(name = "descripcio")
+    @Size(max = 255)
+    private String descripcio;
+
+    public Modalitat() {
+    }
+    
+    
+
+  
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getDescripcio() {
+        return descripcio;
+    }
+
+    public void setDescripcio(String descripcio) {
+        this.descripcio = descripcio;
+    }
+    
+    
+    
+    
     
 }
